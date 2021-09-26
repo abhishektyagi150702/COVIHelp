4a09d4f621bdcd3604390514a0d54de130d6351b
Abhishek Tyagi <abhishektyagi150702@gmail.com>
2021-09-26 11:38:58 +0530

    Code till the interview I have done

--- src/components/common/navbar/navbar.js
+++ src/components/common/navbar/navbar.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css"
+import SearchIcon from "../../../assets/Icons/searchIcon.svg"
 
 export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg  align-items-center">
   <div className="container-fluid">
-    <NavLink className="navbar-brand fs-2" to="/"><span className="theme-logo-p1" style={{fontFamily: "showcard_Gothic"}}>COVI</span><span className="theme-logo-p2">Help</span></NavLink>
+    <NavLink className="navbar-brand ms-5 fs-2" to="/"><span className="theme-logo-p1" style={{fontFamily: "showcard_Gothic"}}>COVI</span><span className="theme-logo-p2">Help</span></NavLink>
     <button className="navbar-toggler navbar-light"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon " ></span>
     </button>
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
-    <form className="d-flex ms-4 w-50">
-        <input className="form-control w-75 me-2 " type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
+    <form className="d-flex ms-5 searchForm rounded-pill bg-light">
+        <input className="form-control inputSearch me-2 " type="search" placeholder="Search" aria-label="Search" />
+        <img src={SearchIcon} className="me-2"  alt="" />
       </form>
-      <ul className="navbar-nav nav-menu  mb-2  mb-lg-0 align-items-center justify-content-end">
+      <ul className="navbar-nav nav-menu mx-auto  mb-2  mb-lg-0 align-items-center justify-content-end">
         <li className="nav-item px-3">
           <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
         </li>
@@ -27,10 +28,10 @@ export default function Navbar() {
           <NavLink className="nav-link" to="/contact">Contac Us </NavLink>
         </li>
         <li className="nav-item px-2">
-          <NavLink className="nav-link" to="/contact">Log In</NavLink>
+          <NavLink className="nav-link" to="/login">Log In</NavLink>
         </li>
         <li className="nav-item px-2">
-          <NavLink className="nav-link" to="/contact">Create Account</NavLink>
+          <NavLink className="nav-link" to="/signup"><button className="btn theme-skyblue" style={{color: "white"}}>Create Account</button></NavLink>
         </li>
       </ul>
     
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "./heroSection/banner";
 
 export const Home = () => {
     return (
         <>
-        <h1>This is Home page</h1>
+        <Banner className="theme-bg" />
         </>
     )
 }
